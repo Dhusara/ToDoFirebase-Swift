b8860900439da9228e479e3e946b77a15caf83c0
Sergei Demianenko <siriysgrey@gmail.com>
2020-03-04 23:46:10 +0200

    firebase

---
+++ ToDoFirebase/Models/Task.swift
@@ -0,0 +1,9 @@
+//
+//  Task.swift
+//  ToDoFirebase
+//
+//  Created by Serhii Demianenko on 04.03.2020.
+//  Copyright © 2020 Serhii Demianenko. All rights reserved.
+//
+
+import Foundation
---
+++ ToDoFirebase/Models/User.swift
@@ -0,0 +1,9 @@
+//
+//  User.swift
+//  ToDoFirebase
+//
+//  Created by Serhii Demianenko on 04.03.2020.
+//  Copyright © 2020 Serhii Demianenko. All rights reserved.
+//
+
+import Foundation
